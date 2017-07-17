88aca82d20d9b8b8b2d2991d7b8a6f7784022e5b
Zhanpeng DIAO <diaozhanpeng1015@gmail.com>
2017-07-17 21:11:36 +1000

    8 Starting thread II

--- com.diao.currency/src/com/diao/BasicMultiThreading/App.java
+++ com.diao.currency/src/com/diao/BasicMultiThreading/App.java
@@ -4,24 +4,34 @@
  */
 package com.diao.BasicMultiThreading;
 
-class Runner1 implements Runnable{
+class Runner1 extends Thread{
 
     @Override
     public void run() {
         // going to execute in a distinct thread
         for(int i = 0; i < 10; i++) {
             System.out.println("Runner1: " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
 }
 
-class Runner2 implements Runnable{
+class Runner2 extends Thread{
 
     @Override
     public void run() {
         for(int i = 0; i < 10; i++) {
             System.out.println("Runner2: " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -32,12 +42,16 @@ public class App {
     public static void main(String[] args) {
 
         // initiate thread
-        Thread thread1 = new Thread(new Runner1());
-        Thread thread2 = new Thread(new Runner2());
+//        Thread thread1 = new Thread(new Runner1());
+//        Thread thread2 = new Thread(new Runner2());
+        Runner1 runner1 = new Runner1();
+        Runner2 runner2 = new Runner2();
 
         // start() will call the run() method within the argument
-        thread1.start();
-        thread2.start();
+//        thread1.start();
+//        thread2.start();
+        runner1.start();
+        runner2.start();
 
     }
 }
