e02efb1b772cb965ab4a13060e39fe4bcbf55a74
Zhanpeng DIAO <diaozhanpeng1015@gmail.com>
2017-07-17 21:41:36 +1000

    10 volatile

--- com.diao.currency/src/com/diao/BasicMultiThreading/App.java
+++ com.diao.currency/src/com/diao/BasicMultiThreading/App.java
@@ -1,64 +1,53 @@
-/**
- * Created by ZhanpengDiao on 17/7/17.
- * Runnable
- */
 package com.diao.BasicMultiThreading;
 
-class Runner1 extends Thread{
+/**
+ * Created by ZhanpengDiao on 17/7/17.
+ */
+
+class Worker implements Runnable {
+
+    public boolean isTerminated() {
+        return isTerminated;
+    }
+
+    public void setTerminated(boolean terminated) {
+        isTerminated = terminated;
+    }
+
+    // volatile: read from RAM instead of cache
+    // performance lost
+    private volatile boolean isTerminated = false;
 
     @Override
     public void run() {
-        // going to execute in a distinct thread
-        for(int i = 0; i < 10; i++) {
-            System.out.println("Runner1: " + i);
+
+        while(!isTerminated) {
+            System.out.println("Hello from worker.");
+
             try {
-                Thread.sleep(100);
+                Thread.sleep(300);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+
     }
-
-}
-
-class Runner2 extends Thread{
-
-    @Override
-    public void run() {
-        for(int i = 0; i < 10; i++) {
-            System.out.println("Runner2: " + i);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
 }
 
 public class App {
 
-    // main/application thread
     public static void main(String[] args) throws InterruptedException {
 
-        // initiate thread
-//        Thread thread1 = new Thread(new Runner1());
-//        Thread thread2 = new Thread(new Runner2());
-        Runner1 runner1 = new Runner1();
-        Runner2 runner2 = new Runner2();
+        Worker worker1 = new Worker();
 
-        // start() will call the run() method within the argument
-//        thread1.start();
-//        thread2.start();
-        runner1.start();
-        runner2.start();
+        Thread thread1 = new Thread(worker1);
+        thread1.start();
 
-        // wait for the thread to die
-        runner1.join();
-        runner2.join();
+        Thread.sleep(3000);
 
-        System.out.println("All tasks finished.");
+        worker1.setTerminated(true);
+        System.out.println("Finished.");
+
     }
-}
 
+}
---
+++ com.diao.currency/src/com/diao/BasicMultiThreading/StartingThread.java
@@ -0,0 +1,64 @@
+/**
+ * Created by ZhanpengDiao on 17/7/17.
+ * Runnable
+ */
+package com.diao.BasicMultiThreading;
+
+class Runner1 extends Thread{
+
+    @Override
+    public void run() {
+        // going to execute in a distinct thread
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Runner1: " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+class Runner2 extends Thread{
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Runner2: " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+public class StartingThread {
+
+    // main/application thread
+    public static void main(String[] args) throws InterruptedException {
+
+        // initiate thread
+//        Thread thread1 = new Thread(new Runner1());
+//        Thread thread2 = new Thread(new Runner2());
+        Runner1 runner1 = new Runner1();
+        Runner2 runner2 = new Runner2();
+
+        // start() will call the run() method within the argument
+//        thread1.start();
+//        thread2.start();
+        runner1.start();
+        runner2.start();
+
+        // wait for the thread to die
+        runner1.join();
+        runner2.join();
+
+        System.out.println("All tasks finished.");
+    }
+}
+
